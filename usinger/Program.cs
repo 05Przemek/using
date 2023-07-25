e5edf5d737d67a20708a308c622865b726a77e49
05Przemek <przemochmielowiec@gmail.com>
2023-07-25 14:47:53 +0200

    using

---
+++ usinger/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+using (HttpClient client = new HttpClient())
+{
+    string apiUrl = @"URL: https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,political,racist,explicit";
+
+    string response = await Client.GetStringAsync(apiUrl);
+}
+}
